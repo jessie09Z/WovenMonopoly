98c57a5b128099aa8675c55f292755408b22cff9
jessie09Z <liumingvic@gmail.com>
2025-01-21 02:53:01 +1100

    Add BoardSpace.cs

---
+++ WovenMonopoly/BoardSpace.cs
@@ -0,0 +1,10 @@
+﻿namespace WovenMonopoly
+{
+    class BoardSpace
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public string Colour { get; set; }
+    }
+}
