a107d0348854cd4ecd5127ce1edba8bce1502964
jessie09Z <liumingvic@gmail.com>
2025-01-21 02:53:22 +1100

    Add Player.cs

---
+++ WovenMonopoly/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace WovenMonopoly
+{
+    class Player
+    {
+        public string Name { get; }
+        public int Position { get; set; } = 0;
+        public int Money { get; set; } = 16;
+        public HashSet<string> OwnedProperties { get; } = new HashSet<string>();
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public void Move(int steps, List<BoardSpace> board)
+        {
+            Position = (Position + steps) % board.Count;
+            if (Position == 0 && steps > 0)
+            {
+                Money += 1; // Collect $1 when passing GO
+            }
+        }
+
+        public void PayRent(Player owner, int rent)
+        {
+            Money -= rent;
+            owner.Money += rent;
+        }
+    }
+}
