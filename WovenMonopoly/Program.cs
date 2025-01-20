db7df95aca404c0da33cecfb5d15cd78d2ae0d78
jessie09Z <liumingvic@gmail.com>
2025-01-21 02:54:13 +1100

    Add Program.cs

---
+++ WovenMonopoly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace WovenMonopoly
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string board = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty,
+                @"Data\board.json");
+            string rolls = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty,
+                @"Data\rolls_2.json");
+            var game = new MonopolyGame(board, rolls);
+            game.PlayGame();
+            game.PrintFinalStandings();
+        }
+    }
+}
