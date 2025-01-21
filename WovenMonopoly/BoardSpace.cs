de144882ef454052ebe1f24da73788bb978e8aa1
jessie09Z <liumingvic@gmail.com>
2025-01-21 13:13:11 +1100

    add constructor for BoardSpace

--- WovenMonopoly/BoardSpace.cs
+++ WovenMonopoly/BoardSpace.cs
@@ -1,7 +1,15 @@
 ﻿namespace WovenMonopoly
 {
-    class BoardSpace
+    public class BoardSpace
     {
+        public BoardSpace(string name, string type, int price, string colour)
+        {
+            Name = name;
+            Type = type;
+            Price = price;
+            Colour = colour;
+        }
+
         public string Name { get; set; }
         public string Type { get; set; }
         public int Price { get; set; }
