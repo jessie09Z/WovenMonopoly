b530d62d21c2eccdbc12399c19278a159c84ab97
jessie09Z <liumingvic@gmail.com>
2025-01-21 13:18:53 +1100

    fix Move logic for player

--- WovenMonopoly/Player.cs
+++ WovenMonopoly/Player.cs
@@ -6,6 +6,7 @@ namespace WovenMonopoly
     {
         public string Name { get; }
         public int Position { get; set; } = 0;
+        public bool IsFirstMove { get; set; } = true;
         public int Money { get; set; } = 16;
         public HashSet<string> OwnedProperties { get; } = new HashSet<string>();
 
@@ -16,11 +17,36 @@ namespace WovenMonopoly
 
         public void Move(int steps, List<BoardSpace> board)
         {
-            Position = (Position + steps) % board.Count;
-            if (Position == 0 && steps > 0)
+            //when everybody starts on GO
+            if (IsFirstMove)
             {
-                Money += 1; // Collect $1 when passing GO
+                IsFirstMove = false; // skip Collect $1 when everybody starts on GO
+                Position += steps;
             }
+            else
+            {
+                if (Position == 0)
+                {
+                    StepForward(steps, board);
+                    Money += 1; // when player is at GO now, will collect $1 when passing GO
+                }
+                else
+                {
+                    // when player is over GO after stepping
+                    var isOverGo = Position + steps > board.Count;
+                    if (isOverGo)
+                    {
+                        Money += 1;
+                    }
+
+                    StepForward(steps, board);
+                }
+            }
+        }
+
+        private void StepForward(int steps, List<BoardSpace> board)
+        {
+            Position = (Position + steps) % board.Count;
         }
 
         public void PayRent(Player owner, int rent)
